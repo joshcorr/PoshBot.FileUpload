49a6e44e2d48b69128427b8feb140297bb2b9b2b
Joshua Corrick <Joshua.corrick@live.com>
2021-06-27 00:52:32 -0400

    Initial Commit

---
+++ PoshBot.FileUpload/PoshBot.FileUpload.psd1
@@ -0,0 +1,38 @@
+﻿@{
+RootModule = 'PoshBot.FileUpload.psm1'
+ModuleVersion = '0.0.1'
+GUID = 'e5109c16-9f7c-4c01-ad1c-5b7b0668a328'
+Author = 'Josh Corrick (@joshcorr)'
+CompanyName = 'blog.corrick.io'
+Copyright = '(c) 2021 Josh Corrick (@joshcorr). All rights reserved.'
+Description = 'PoshBot plugin for uploading files'
+PowerShellVersion = '5.0.1'
+RequiredModules = @('PoshBot')
+FunctionsToExport = @('uploadfile')
+CmdletsToExport = @()
+VariablesToExport = @()
+AliasesToExport = @()
+PrivateData = @{
+		Permissions = @(
+			@{
+				Name = 'Read'
+				Description = 'Can run all Get commands'
+			}
+			@{
+				Name = 'Write'
+				Description = 'Can run all Set, Write, Update commands'
+			}
+			@{
+				Name = 'Execute'
+				Description = 'Can run all Invoke, Start commands'
+			}
+		)
+		PSData = @{
+			Tags = @('PoshBot','ChatOps','Files')
+			LicenseUri = 'https://raw.githubusercontent.com/joshcorr/PoshBot.FileUpload/main/LICENSE'
+			ProjectUri = 'https://github.com/joshcorr/PoshBot.FileUpload'
+			ReleaseNotes = 'https://raw.githubusercontent.com/joshcorr/PoshBot.FileUpload/main/CHANGELOG.md'
+			# Prerelease = ''
+		}
+	}
+}
---
+++ PoshBot.FileUpload/PoshBot.FileUpload.psm1
@@ -0,0 +1,50 @@
+﻿#Functions written below:
+function uploadfile {
+    <#
+    .SYNOPSIS
+        PoshBot function to upload a local file (or one the service account can access)
+    .EXAMPLE
+        !uploadfile [-souce [//path/to/something|C:\path\locally] [-Title <someFileName>] [-DM] [-Content]]
+    #>
+    [PoshBot.BotCommand(
+        CommandName = 'uploadfile',
+        Aliases = ('upload')
+    )]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]$source,
+        [String]$title,
+        [switch]$dm,
+        [switch]$content
+    )
+    try {
+        if (Test-Path -Path $source) {
+            if ($content){
+                $splatParams = @{
+                    Content = $(Get-Content -Path $source)
+                }
+            } else {
+                $splatParams = @{
+                    Path = $source
+                    KeepFile = $true
+                }
+            }
+            switch ($PSBoundParameters.Keys){
+                'dm' { $splatParams.DM = $true }
+                'title' {$splatParams.Title =  $title}
+            }
+
+            New-PoshBotFileUpload @splatParams
+        } else {
+            New-PoshBotCardResponse -Type Error -Text "File does not exist at the path: $source."
+        }
+    } catch {
+        New-PoshBotCardResponse -Type Error -Text "Unable to upload: $source."
+    }
+}
+
+
+
+# Export all functions for poshbot
+Export-ModuleMember *
